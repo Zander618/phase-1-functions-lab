96fc75f3ee0e32971095007c8b5b804c44850438
Zachary Sirna <zsirna@yahoo.com>
2022-02-23 16:36:22 -0500

    commit tree 0f8d2a0e046641ab842b52f1e7d55e2c3287555c

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block){
+    if (block > 42){
+    return block - 42;
+    }else{
+        return 42 - block;
+    }
+};
+
+function distanceFromHqInFeet(number){
+   return distanceFromHqInBlocks(number) * 264;
+};
+
+function distanceTravelledInFeet(start, destination){
+    if (destination > start) {
+    return (destination - start) * 264;
+    } else if (destination < start) {
+        return (start - destination) * 264;
+    }
+};
+
+function calculatesFarePrice(beggining , end){
+    if (distanceTravelledInFeet(beggining, end) < 400){
+        return 0;
+    } else if (distanceTravelledInFeet(beggining, end) > 400 && distanceTravelledInFeet(beggining, end) < 2000){ 
+        return (distanceTravelledInFeet(beggining, end) - 400) * .02;
+    } else if (distanceTravelledInFeet(beggining, end) > 2000 && distanceTravelledInFeet(beggining, end) < 2500){ 
+        return 25;
+    }  else if (distanceTravelledInFeet(beggining, end) > 2500 ) {
+        return "cannot travel that far";
+    }
+};
